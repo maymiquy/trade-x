8ab23eba3f8ccf33852f54aa68de07ca1b52ae4a
MiqdamFerdiansyahHambali <miqd22166ti@student.nurulfikri.ac.id>
2024-08-13 21:42:30 +0700

    chore: adding core dependencies

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,4 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 /**
  * Metro configuration
@@ -6,6 +6,27 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+const defaultConfig = getDefaultConfig(__dirname);
+
+const {
+    resolver: { sourceExts, assetExts },
+} = getDefaultConfig(__dirname);
+
+const config = {
+    transformer: {
+        getTransformOptions: async () => ({
+            transform: {
+                experimentalImportSupport: false,
+                inlineRequires: true,
+            },
+        }),
+        babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    },
+    resolver: {
+        assetExts: assetExts.filter(ext => ext !== 'svg'),
+        sourceExts: [...sourceExts, 'svg'],
+    },
+};
+
+module.exports = mergeConfig(defaultConfig, config);
