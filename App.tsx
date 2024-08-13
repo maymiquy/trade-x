228dcb7c0fab5d245c2b4ac6efbddda61416efa3
MiqdamFerdiansyahHambali <miqd22166ti@student.nurulfikri.ac.id>
2024-08-14 01:59:44 +0700

    fix: add missing react-redux provider

--- App.tsx
+++ App.tsx
@@ -7,9 +7,15 @@
 
 import React from "react";
 import Navigation from "./src/navigation/Navigation";
+import { Provider } from "react-redux";
+import { store } from "./src/hooks/store";
 
 const App = () => {
- return <Navigation />;
+ return (
+  <Provider store={store}>
+   <Navigation />
+  </Provider>
+ );
 };
 
 export default App;
