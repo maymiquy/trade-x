ed80b1199bccf448ece49e225af60b8ca6202de4
MiqdamFerdiansyahHambali <miqd22166ti@student.nurulfikri.ac.id>
2024-08-14 21:32:41 +0700

    chore: adding utility func & assets font

---
+++ src/utils/constants/Fonts.ts
@@ -0,0 +1,11 @@
+export enum FONTS {
+ Black = "Roboto-Black",
+ Bold = "Roboto-Bold",
+ Light = "Roboto-Light",
+ Medium = "Roboto-Medium",
+ Regular = "Roboto-Regular",
+ Thin = "Roboto-Thin",
+ Number = "Manrope-Regular",
+ NumberSemiBold = "Manrope-SemiBold",
+ Lato = "Lato-Regular",
+}
